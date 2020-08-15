7128fc7ea8f378db86acf126c040598877a65e55
YourRaccon <rutrexit@mail.ru>
2020-08-16 01:29:39 +0300

    DZ 6

---
+++ 1. Основы языка C#/DZ6/DZ2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    //Корбун Т.И.
+    /*
+     Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. 
+     а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум. 
+     Использовать массив (или список) делегатов, в котором хранятся различные функции.
+     б) *Переделать функцию Load, чтобы она возвращала массив считанных значений. 
+     Пусть она возвращает минимум через параметр (с использованием модификатора out). 
+     */
+
+    public delegate double Fun(double x);
+
+    class Program
+    {
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double F2(double x)
+        {
+            return x + 1;
+        }
+
+        public static double F3(double x)
+        {
+            return x / 10 + 4;
+        }
+
+        public static double F4(double x)
+        {
+            return (x - 6) / x;
+        }
+
+        public static void SaveFunc(Fun F, string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        public static double[] Load(string fileName, out double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            min = double.MaxValue;
+            List<double> mas = new List<double>();
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                mas.Add(d);
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return mas.ToArray();
+        }
+
+        public static void StartService(Fun F)
+        {
+            Console.Write("Укажите диапазон поиска минимума функции.\nЛевая граница: ");
+            double a, b;
+            if (!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Некорректно задана граница диапазона. Необходимо задать целое число, либо число в виде десятичной дроби. Возврат в меню.");
+                return;
+            }
+            Console.Write("Правая граница: ");
+            if (!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Некорректно задана граница диапазона. Необходимо задать целое число, либо число в виде десятичной дроби. Возврат в меню.");
+                return;
+            }
+            if(a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            SaveFunc(F, "data.bin", a, b, 0.5);
+            double min;
+            Load("data.bin", out min);
+            Console.WriteLine("Минимум функции равен: {0}\n", min);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Fun> functions = new List<Fun>();
+            functions.Add(F1);
+            functions.Add(F2);
+            functions.Add(F3);
+            functions.Add(F4);
+
+            while (true)
+            {
+                Console.WriteLine("Программа нахождения минимума функции. Введите номер одной из предложенных ниже функций:\n" +
+                    "1)x * x - 50 * x + 10" +
+                    "\n2)x + 1" +
+                    "\n3)x / 10 + 4" +
+                    "\n4)(x - 6) / x");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1": 
+                    case "2": 
+                    case "3": 
+                    case "4": StartService(functions[int.Parse(input) - 1]); break;
+                    default: Console.WriteLine("Завершение работы программы"); Console.ReadKey(); return;
+                }
+            }
+        }
+    }
+}
---
+++ 1. Основы языка C#/DZ6/DZ4/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4
+{
+    //Корбун Т.И.
+    /*
+     **Считайте файл различными способами. 
+     * Смотрите “Пример записи файла различными способами”. 
+     * Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), строку для StreamReader и массив int для BinaryReader.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long kbyte = 1024;
+            long mbyte = 1024 * kbyte;
+            long gbyte = 1024 * mbyte;
+            long size = kbyte;
+            //Write FileStream
+            //Write BinaryStream
+            //Write StreamReader/StreamWriter
+            //Write BufferedStream
+
+            //Примеры различной записи
+            Console.WriteLine("Запись:");
+            Console.WriteLine("FileStream. Milliseconds:{0}", FileStreamSampleWrite("bigdata0.bin", size));
+            Console.WriteLine("BinaryStream. Milliseconds:{0}", BinaryStreamSampleWrite("bigdata1.bin", size));
+            Console.WriteLine("StreamWriter. Milliseconds:{0}", StreamWriterSample("bigdata2.bin", size));
+            Console.WriteLine("BufferedStream. Milliseconds:{0}", BufferedStreamSampleWrite("bigdata3.bin", size));
+
+            //Примеры различного чтения
+            Console.WriteLine("\nЧтение:");
+            byte[] tmp1 = FileStreamSampleRead("bigdata0.bin", size);
+            Console.WriteLine("{0}, size: {1}", tmp1, tmp1.Length);
+            int[] tmp2 = BinaryStreamSampleRead("bigdata1.bin", size);
+            Console.WriteLine("{0}, size: {1}", tmp2, tmp2.Length);
+            string tmp3 = StreamReaderSample("bigdata2.bin");
+            Console.WriteLine("{0}, length: {1}", tmp3, tmp3.Length);
+            byte[] tmp4 = BufferedStreamSampleRead("bigdata3.bin", size);
+            Console.WriteLine("{0}, size: {1}", tmp4, tmp4.Length);
+
+            Console.ReadKey();
+        }
+
+        static long FileStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            for (int i = 0; i < size; i++)
+                fs.WriteByte(0);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] FileStreamSampleRead(string filename, long size)
+        {
+            List<byte> result = new List<byte>();
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            for (int i = 0; i < size; i++)
+                result.Add((byte)fs.ReadByte());
+            fs.Close();
+            return result.ToArray();
+        }
+
+        static long BinaryStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            for (int i = 0; i < size; i++)
+                bw.Write((byte)0);
+            bw.Close();
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static int[] BinaryStreamSampleRead(string filename, long size)
+        {
+            List<int> result = new List<int>();
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            for (int i = 0; i < size; i++)
+                result.Add(br.Read());
+            br.Close();
+            fs.Close();
+            return result.ToArray();
+        }
+
+        static long StreamWriterSample(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < size; i++)
+                sw.Write(0);
+            sw.Close();
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static string StreamReaderSample(string filename)
+        {
+            string result = "";
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            result = sr.ReadToEnd();
+            sr.Close();
+            fs.Close();
+            return result;
+        }
+
+        static long BufferedStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            int countPart = 4;//количество частей
+            int bufsize = (int)(size / countPart);
+            byte[] buffer = new byte[size];
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            //bs.Write(buffer, 0, (int)size);//Error!
+            for (int i = 0; i < countPart; i++)
+                bs.Write(buffer, 0, (int)bufsize);
+            bs.Close();
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] BufferedStreamSampleRead(string filename, long size)
+        {
+            List<byte> result = new List<byte>();
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int countPart = 4;//количество частей
+            int bufsize = (int)(size / countPart);
+            byte[] buffer = new byte[size];
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            //bs.Write(buffer, 0, (int)size);//Error!
+            for (int i = 0; i < countPart; i++)
+            {
+                bs.Read(buffer, 0, (int)bufsize);
+                result.AddRange(buffer);
+            }
+            bs.Close();
+            fs.Close();
+            return result.ToArray();
+        }
+    }
+}
+
