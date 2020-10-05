5a6f94371bc1093d0ba11713d552ce2dfecca255
YourRaccon <rutrexit@mail.ru>
2020-10-05 21:05:48 +0300

    DZ 4

---
+++ 2. Введение в Unity/DZ4/Игровой проект/Скрипты/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Collider _collider;
+    private Animator _animator;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _collider = gameObject.GetComponent<Collider>();
+        _animator = gameObject.GetComponent<Animator>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void Open()
+    {
+        _animator.SetTrigger("Open");
+        _audioSource.Play();
+    }
+}
